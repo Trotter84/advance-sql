42a1a81d172384231abe0ee87d1131f099274053
Daniel Trotter <flare84@gmail.com>
2018-09-25 10:52:40 -0600

    Available.js

---
+++ client/src/components/Available.js
@@ -0,0 +1,19 @@
+import React from 'react'
+import axios from 'axios'
+import {
+  List,
+  Header,
+  Table,
+} from 'semantic-ui-react'
+
+class Available extends React.Component {
+  componentDidMount() {
+    axios.get('/api/properties')
+      .then( res => { debugger } )
+  }
+  render() {
+    return null
+  }
+}
+
+export default Available
