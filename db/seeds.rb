51428f1eff865fbd606361147531477f8f1cfaad
Daniel Trotter <flare84@gmail.com>
2018-09-25 10:03:41 -0600

    seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,55 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+citties = [
+ 'Sandy',
+ 'Draper',
+ 'SLC',
+ 'Orem',
+ 'Provo',
+ 'Ogden',
+ 'Layton',
+ 'Midvale',
+ 'Murray'
+]
+
+100.times do
+  a = Agent.create(
+    first_name: Faker::Name.first_name,
+    last_name: Faker::Name.last_name,
+    email: Faker::Internet.email,
+    phone: Faker::PhoneNumber.cell_phone
+  )
+
+  50.times do
+    num_citties = rand(0..citties.length - 1);
+    Buyer.create(
+      first_name: Faker::Name.first_name,
+      last_name: Faker::Name.last_name,
+      email: Faker::Internet.email,
+      phone: Faker::PhoneNumber.cell_phone,
+      max_price: rand(99000..1500000),
+      citties: citties.sample(num_citties),
+      agent_id: a.id
+    )
+  end
+
+  50.times do
+    sold = Faker::Boolean.boolean(0.3)
+    price = rand(99000..1500000)
+    percent_change = (-3..3).to_a.sample.to_f / 100
+    sold_price = sold ? price * (1 + percent_change) : nil
+    p = Property.create(
+      price: price,
+      sold: sold,
+      sold_price: sold_price,
+      beds: rand(1..8),
+      baths: rand(1..8),
+      sq_ft: rand(1000..7000),
+      agent_id: a.id
+  )
+
+  p.create_address(
+    street: Faker::Address.street_address,
+    zip: Faker::Address.zip_code,
+    city: citties.sample
+  )
+ end
+end
